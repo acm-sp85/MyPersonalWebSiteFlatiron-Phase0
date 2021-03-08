e5be433d8b9c6c771923d3b4a1d8542ffd9945aa
acm-sp85 <acontell@gmail.com>
2021-03-08 18:43:35 -0500

    Links and GIF added

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeHeader(){
+
+    let header = document.querySelector('h1');
+
+    if (header.innerHTML === "Alex Contell"){
+        header.innerHTML = "Francisco Contell";
+    } else {
+        header.innerHTML = "Alex Contell";
+    }
+
+    
+}
+
+
+
